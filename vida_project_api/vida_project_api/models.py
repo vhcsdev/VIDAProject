79e4e27cb5a4ed652dc1fd99acf9a48a97cd0f9b
vhcsdev <victorwise@hotmail.com>
2025-07-01 10:58:16 -0300

    fix: Foi alterado a versão do python usada no projeto por conta de incompatibilidade as libs usada no modelo de reconhecimento de voz.

---
+++ vida_project_api/vida_project_api/models.py
@@ -0,0 +1,6 @@
+
+
+class User:
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(30))
+    email: Mapped[str] = mapped_column(String(30), unique=True)
