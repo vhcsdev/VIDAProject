b85acca580c9388781fc4d9be83f1eed2c2f0619
Pablo-R-L <99687142+Pablo-R-L@users.noreply.github.com>
2025-07-08 21:51:35 -0300

    Verification Rout added

---
+++ VIDAProject/vida_project_api/vida_project_api/app.py
@@ -0,0 +1,53 @@
+from fastapi import FastAPI, HTTPException, Depends
+from sqlalchemy import select
+
+
+from vida_project_api.models import User
+from vida_project_api.schemas import UserResponseSchema, UserSchema
+from vida_project_api.settings import Settings
+from vida_project_api.database import get_session
+from vida_project_api.voiceRecording import  verify_voice
+
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def hello_world():
+    return {"message": "Hello, World!"}
+
+@app.get("/health")
+def health_check():
+    return {"status": "healthy"}
+
+@app.post("/users", status_code=201, response_model=UserResponseSchema)
+def create_user(user: UserSchema, session=Depends(get_session)):
+    existing_user = session.scalar(select(User).where((User.username == user.username) | (User.email == user.email)))
+    if existing_user:
+        if existing_user.username == user.username:
+            raise HTTPException(status_code=409, detail="Username already exists")
+        elif existing_user.email == user.email:
+            raise HTTPException(status_code=409, detail="Email already exists")
+
+    new_user = User(username=user.username, email=user.email, voice=user.voice)
+    session.add(new_user)
+    session.commit()
+    session.refresh(new_user)
+    return new_user
+
+@app.post("/authenticate", status_code=200)
+def authenticate_user(user: UserSchema, session=Depends(get_session)):
+    existing_user = session.scalar(select(User).where((User.username == user.username) | (User.email == user.email)))
+    if not existing_user:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    if existing_user.username != user.username or existing_user.email != user.email:
+        raise HTTPException(status_code=401, detail="Invalid credentials")
+
+    try:
+        return {"message": verify_voice(user.email), "user_id": existing_user.id}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+    return {"message": "Authentication successful", "user_id": existing_user.id}
---
+++ VIDAProject/vida_project_api/vida_project_api/voiceRecording.py
@@ -0,0 +1,144 @@
+import os
+import sys
+import warnings
+import wave
+
+import pyaudio
+import torch
+import torchaudio
+from speechbrain.inference import SpeakerRecognition
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import io
+
+import migration
+
+warnings.filterwarnings("ignore", category=FutureWarning)
+
+verification = SpeakerRecognition.from_hparams(
+                        source="pretrained_models/spkrec",
+                        savedir="pretrained_models/spkrec")
+
+
+def recording():  
+    format = pyaudio.paInt16
+    channel = 1
+    rate = 44100
+    recordingDuration = 5
+
+    p = pyaudio.PyAudio()
+
+    stream = p.open(format=format,
+                    channels=channel,
+                    rate=rate,
+                    input=True,
+                    frames_per_buffer=1024)
+
+    frames = []
+
+    print(f"Recording duration: {recordingDuration}s")
+
+    aux = 0
+    for i in range(0, int(rate / 1024 * recordingDuration)):
+        data = stream.read(1024)
+        frames.append(data)
+        if int(i % ((rate / 1024 * recordingDuration) / recordingDuration)) == 0:
+            print(f"{aux}s")
+            aux += 1
+
+    print("Finish recording.")
+
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+    buffer = io.BytesIO()
+    with wave.open(buffer, 'wb') as wf:
+        wf.setnchannels(channel)
+        wf.setsampwidth(p.get_sample_size(format))
+        wf.setframerate(rate)
+        wf.writeframes(b''.join(frames))
+    buffer.seek(0)
+    return buffer
+
+
+def extract_embedding(verification, file_path):
+    waveform, sample_rate = torchaudio.load(file_path)
+    signal = waveform.squeeze(0).unsqueeze(0)
+    lengths = torch.tensor([1.0])
+
+    embedding = verification.encode_batch(signal, lengths)
+    return embedding.squeeze(0)
+
+
+
+
+
+def verify_voice(userEmail):
+    global verification
+    if verification is None:
+        verification = SpeakerRecognition.from_hparams(
+            source="pretrained_models/spkrec",
+            savedir="pretrained_models/spkrec"
+    )
+    newVoice = recording()  
+    newVoice = extract_embedding(verification, newVoice)
+
+    # pega uma serie de vozes desse usuario do banco de dados
+    secretVoices = migration.autentication(userEmail)
+
+    # acho q o ideal seria pegar uma media de todos os scores
+    score = 0
+    if secretVoices is not None and len(secretVoices) > 0:
+        for v in secretVoices:
+            veryVoice = torch.load(io.BytesIO(v.voice_module))
+            score += verification.similarity(newVoice, veryVoice)
+        score /= len(secretVoices)
+        return similarity(score)
+    else:
+        return "No secret voices found for this user."
+
+
+def new_secret_voice(userEmail):
+    global verification
+    if verification is None:
+        verification = SpeakerRecognition.from_hparams(
+            source="pretrained_models/spkrec",
+            savedir="pretrained_models/spkrec"
+        )
+    
+    newVoice = recording()  # Da pra tirar o file name?
+    newVoice = extract_embedding(verification, newVoice)
+
+    # salva la no banco de dados
+    bufferTensor = io.BytesIO()
+    torch.save(newVoice, bufferTensor)
+    byteTensor = bufferTensor.getvalue()
+    migration.add_new_voice(byteTensor, userEmail)
+
+
+min_value = 0.7
+
+
+def similarity(score):
+    prediction = score > min_value
+    print(f"Score: {score}, Match: {prediction}\n")
+
+    if (prediction):
+        return "Valid authentication"
+    else:
+        return "Invalid voice"
+
+# similarity(score)
+
+# teste basico
+# s = 0
+# while s != 90:
+#     s = int(input("bota algo ai"))
+#     print(s)
+#     if s == 0:
+#         print("gravar")
+#         new_secret_voice(verification, "Endibaberl@lascapedra.com")
+#     elif s == 1:
+#         print("verificar")
+#         verify_voice("Endibaberl@lascapedra.com")
